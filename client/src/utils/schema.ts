72f02dd5473966fe0e71d57ddd28d2958790206f
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-10 20:15:41 +0600

    feat:setup schema vaildation
    schema for user input form validation
    
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

---
+++ client/src/utils/schema.ts
@@ -0,0 +1,45 @@
+import * as yup from 'yup';
+
+const emailSchema = yup
+	.string()
+	.required('email address required')
+	.email('please provide valid email address')
+	.lowercase()
+	.trim();
+
+const passwordSchema = yup
+	.string()
+	.required('password required')
+	.min(6, 'password is too short minmun 6 characters long')
+	.max(29, 'password is too long maximun 29 characters long');
+
+const confirmPasswordSchema = yup
+	.string()
+	.oneOf([yup.ref('password'), null], 'Passwords must match')
+	.required('confirm password required')
+	.min(6, 'confirm password is too short minmun 6 characters long')
+	.max(29, 'confirm password is too long maximun 29 characters long');
+
+const usernameSchema = yup
+	.string()
+	.required('username required')
+	.min(4, 'user name to short')
+	.max(12, 'user name to long');
+const firstNameSchema = yup.string().required('first name required');
+const lastNameSchema = yup.string().required('last name required');
+
+const loginSchema = yup.object({
+	email: emailSchema,
+	password: passwordSchema
+});
+
+const registerSchema = yup.object({
+	first_name: firstNameSchema,
+	last_name: lastNameSchema,
+	username: usernameSchema,
+	email: emailSchema,
+	password: passwordSchema,
+	confirmPassword: confirmPasswordSchema
+});
+
+export { loginSchema, registerSchema };
