ff168dae20bd8f5eb085f735bbb32c4bdf173e1f
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-22 17:37:29 +0600

    feat: add resolver for update user credentials
    add user data  and password update schema and resolver
    
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

--- server/src/@types/user.ts
+++ server/src/@types/user.ts
@@ -12,3 +12,22 @@ export interface IUserData {
 	password: string;
 	gender: Gender;
 }
+
+export interface IUserUpdate {
+	first_name?: string;
+	last_name?: string;
+	email?: string;
+	username?: string;
+	address?: String;
+}
+
+export interface IUserUpdatePassword {
+	currentPassword: string;
+	newPassword: string;
+}
+
+export interface IUserTokenDecode {
+	id: string;
+	exp: string;
+	iat: string;
+}
--- server/src/resolvers/user.mutation.ts
+++ server/src/resolvers/user.mutation.ts
@@ -1,12 +1,23 @@
+import geoIp from 'geoip-lite';
 import { IResolvers } from 'graphql-tools';
 import { Context } from '../../prisma';
 import { FileUpload } from 'graphql-upload';
 import path from 'path';
 import { v4 as uuid } from 'uuid';
-import { verifyAuthToken } from '../helpers/auth';
+import {
+	comparePassword,
+	getToken,
+	hashPassword,
+	verifyAuthToken
+} from '../helpers/auth';
 import { resizeImage } from '../helpers/image';
-import { getHostUrl } from '../helpers/utils';
+import { getHostUrl, getUserAgent } from '../helpers/utils';
 import { ResizeOptions } from 'sharp';
+import { IUserTokenDecode, IUserUpdate, IUserUpdatePassword } from '../@types/user';
+import { registerSchema, updatePasswordSchema } from '../utils/validatorSchema';
+import { decode, DecodeOptions } from 'jsonwebtoken';
+// @ts-ignore
+import { getClientIp } from 'request-ip';
 
 const userResolvers: IResolvers = {
 	async uploadImage(_, args, cxt: Context, info) {
@@ -48,6 +59,97 @@ const userResolvers: IResolvers = {
 					}
 				});
 			});
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	},
+	async updateUser(_, args: { data: IUserUpdate }, cxt: Context, info) {
+		try {
+			// check user is auth
+			const user = await verifyAuthToken(cxt);
+			// user updateable data key
+			const defaultKey = [
+				'first_name',
+				'last_name',
+				'email',
+				'username',
+				'address'
+			];
+
+			const argsKey: string[] = Object.keys(args.data);
+			// check user input is empty or not in the default key
+			if (!!!argsKey.length) {
+				throw new Error('input must not be empty');
+			}
+
+			//  check any field pass that is not accepted
+			if (argsKey.find(key => !defaultKey.includes(key))) {
+				throw new Error('invalid input value');
+			}
+
+			// get validation schema key
+			const schemaKey = defaultKey.filter(item => argsKey.includes(item));
+
+			const updateUserSchema = registerSchema.pick(schemaKey);
+			await updateUserSchema.validate(args.data);
+
+			const updatedUser = await cxt.prisma.user.update({
+				where: { id: user.id },
+				// @ts-ignore
+				data: args.data
+			});
+			return updatedUser;
+		} catch (err) {
+			console.log(err);
+			throw err;
+		}
+	},
+	async updatePassword(_, args: { data: IUserUpdatePassword }, cxt: Context, info) {
+		try {
+			// check user is auth
+			const user = await verifyAuthToken(cxt);
+			// validate user input
+			await updatePasswordSchema.validate(args.data);
+
+			// check old password match
+			const passwordMatch = await comparePassword(
+				args.data.currentPassword,
+				user.password.trim()
+			);
+			if (passwordMatch) {
+				const hashedPassword = await hashPassword(args.data.newPassword);
+				const ip_address = getClientIp(cxt.request);
+				let details = {};
+				if (ip_address) {
+					details = geoIp.lookup(ip_address) || {};
+				}
+				const token = await getToken(user.id);
+				const expires_at = decode(token) as IUserTokenDecode;
+
+				await cxt.prisma.user.update({
+					where: { id: user.id },
+					data: {
+						password: hashedPassword,
+						password_changed_at: new Date().toISOString(),
+						tokens: {
+							deleteMany: {
+								user_id: user.id
+							},
+							create: {
+								token,
+								agent: getUserAgent(cxt.request.headers['user-agent']),
+								expires_at: new Date(expires_at.exp).toISOString(),
+								ip_address,
+								details: JSON.stringify(details)
+							}
+						}
+					}
+				});
+				return { ...user, token };
+			} else {
+				throw new Error("password don't match");
+			}
 		} catch (err) {
 			console.log(err);
 			throw err;
