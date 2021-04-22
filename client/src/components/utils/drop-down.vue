ef69422f5e5d56eacd2a457bf03b9765aec9ce44
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-22 18:00:20 +0600

    style: add gender dropdown and restyle page
    add gender select dropdown component and restyle the page
    
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

---
+++ client/src/components/utils/drop-down.vue
@@ -0,0 +1,85 @@
+<template>
+	<div>
+		<Menu as="div" class="relative text-left">
+			<div>
+				<MenuButton
+					class="input btn btn-block text-white focus:outline-none focus-visible:ring-3 focus-visible:ring-white focus-visible:ring-opacity-75 capitalize shadow-2xl"
+					:class="error && 'btn-warning'"
+				>
+					{{ gender || 'Gender' }}
+					<i class="eva eva-chevron-down-outline eva-lg"></i>
+				</MenuButton>
+			</div>
+
+			<transition
+				enter-active-class="transition duration-100 ease-out"
+				enter-from-class="transform scale-95 opacity-0"
+				enter-to-class="transform scale-100 opacity-100"
+				leave-active-class="transition duration-75 ease-in"
+				leave-from-class="transform scale-100 opacity-100"
+				leave-to-class="transform scale-95 opacity-0"
+			>
+				<MenuItems
+					class="absolute w-56 mt-2 origin-top-right bg-dark-input rounded-md shadow-lg focus:outline-none p-2"
+				>
+					<div class="p-3">
+						<MenuItem v-slot="{ active }">
+							<button
+								:class="[
+									active && 'bg-neutral',
+									' group flex rounded-md items-center w-full px-2 py-2 text-sm'
+								]"
+								type="button"
+								@click.prevent="$emit('selectedItem', 'male')"
+							>
+								Male
+							</button>
+						</MenuItem>
+					</div>
+					<div class="p-3">
+						<MenuItem v-slot="{ active }">
+							<button
+								:class="[
+									active && 'bg-neutral',
+									'group flex rounded-md items-center w-full px-2 py-2 text-sm'
+								]"
+								type="button"
+								@click.prevent="$emit('selectedItem', 'female')"
+							>
+								Female
+							</button>
+						</MenuItem>
+					</div>
+					<div class="p-3">
+						<MenuItem v-slot="{ active }">
+							<button
+								@click.prevent="$emit('selectedItem', 'other')"
+								:class="[
+									active && 'bg-neutral',
+									'group flex rounded-md items-center w-full px-2 py-2 text-sm'
+								]"
+								type="button"
+							>
+								Other
+							</button>
+						</MenuItem>
+					</div>
+				</MenuItems>
+			</transition>
+		</Menu>
+	</div>
+</template>
+
+<script>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+	props: ['gender', 'error'],
+	components: {
+		Menu,
+		MenuButton,
+		MenuItems,
+		MenuItem
+	}
+});
+</script>
--- client/src/pages/register.vue
+++ client/src/pages/register.vue
@@ -1,227 +1,234 @@
 <template>
 	<default-layout>
-		<div
-			class="container max-w-full mx-auto flex justify-center items-center flex-col flex-1 py-20"
-		>
-			<UserIcon class="w-20 h-20 mx-auto text-green-500" />
-
-			<form
-				class="mt-8 w-full mx-auto max-w-xl grid gap-x-5 grid-cols-2"
-				id="login-form"
-				@submit="onHandleSubmit"
+		<div class="py-20">
+			<div
+				class="bg-neutral mx-auto p-8 rounded-2xl shadow-2xl"
+				style="max-width: 700px"
 			>
-				<div class="py-2">
-					<span class="auth-input-label"> First Name </span>
-					<div class="relative">
-						<input
-							placeholder="Jhon"
-							type="text"
-							class="auth-input"
-							v-model="first_name"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-person-outline"></i>
-						</div>
-					</div>
-					<span v-if="errors.first_name" class="auth-input-error">
-						{{ errors.first_name }}
-					</span>
-				</div>
-				<div class="py-2">
-					<span class="auth-input-label"> Last Name </span>
-					<div class="relative">
-						<input
-							placeholder="Deo"
-							type="text"
-							class="auth-input"
-							v-model="last_name"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-person-outline"></i>
-						</div>
-					</div>
-					<span v-if="errors.last_name" class="auth-input-error">
-						{{ errors.last_name }}
-					</span>
-				</div>
-				<div class="py-2">
-					<span class="auth-input-label"> Email </span>
-					<div class="relative">
-						<input
-							placeholder="user@email.com"
-							type="text"
-							class="auth-input"
-							v-model="email"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-email-outline"></i>
-						</div>
-					</div>
-					<span v-if="errors.email" class="auth-input-error">
-						{{ errors.email }}
-					</span>
-				</div>
-				<div class="py-2">
-					<span class="auth-input-label"> Username </span>
-					<div class="relative">
-						<input
-							placeholder="user@email.com"
-							type="text"
-							class="auth-input"
-							v-model="username"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-email-outline"></i>
-						</div>
-					</div>
-					<span v-if="errors.username" class="auth-input-error">
-						{{ errors.username }}
-					</span>
-				</div>
-				<div class="py-2" x-data="{ show: true }">
-					<span class="auth-input-label">Password</span>
-					<div class="relative">
-						<input
-							id="password"
-							placeholder="................"
-							:type="showPassword ? 'text' : 'password'"
-							class="auth-input"
-							v-model="password"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-lock-outline"></i>
-						</div>
-						<div
-							class="absolute right-0 px-3 rounded-lg flex items-center text-2xl leading-5 text-gray-800 bg-white"
-							@click="showPassword = !showPassword"
-							style="top: 2px; bottom: 2px; right: 2px"
-						>
-							<i
-								:class="`eva ${
-									showPassword
-										? 'eva-eye-off-outline'
-										: 'eva-eye-outline'
-								}`"
-							></i>
-						</div>
-					</div>
-					<span v-if="errors.password" class="auth-input-error">
-						{{ errors.password }}
-					</span>
-				</div>
-				<div class="py-2" x-data="{ show: true }">
-					<span class="auth-input-label">Confirm Password</span>
-					<div class="relative">
-						<input
-							id="confirm-password"
-							placeholder="................"
-							:type="showConfirmPassword ? 'text' : 'password'"
-							class="auth-input"
-							v-model="confirmPassword"
-						/>
-						<div class="auth-input-icon">
-							<i class="eva eva-lock-outline"></i>
-						</div>
-						<div
-							class="absolute right-0 px-3 rounded-lg flex items-center text-2xl leading-5 text-gray-800 bg-white"
-							@click="showConfirmPassword = !showConfirmPassword"
-							style="top: 2px; bottom: 2px; right: 2px"
-						>
-							<i
-								:class="`eva ${
-									showConfirmPassword
-										? 'eva-eye-off-outline'
-										: 'eva-eye-outline'
-								}`"
-							></i>
-						</div>
-					</div>
-					<span v-if="errors.confirmPassword" class="auth-input-error">
-						{{ errors.confirmPassword }}
-					</span>
-				</div>
-				<div class="grid-cols-2 py-2" style="grid-column: 1 / span 2">
-					<div class="relative inline-flex">
-						<svg
-							class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 412 232"
-						>
-							<path
-								d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
-								fill="#648299"
-								fill-rule="nonzero"
+				<UserIcon class="w-20 h-20 mx-auto text-green-300" />
+				<form
+					class="mt-8 grid gap-x-7 grid-cols-2"
+					id="login-form"
+					@submit="onHandleSubmit"
+				>
+					<div class="form-control py-2">
+						<label class="label">
+							<span class="label-text">First Name</span>
+						</label>
+						<div class="relative">
+							<input
+								placeholder="Jhon"
+								type="text"
+								class="auth-input input"
+								v-model="first_name"
 							/>
-						</svg>
-						<select
-							class="border-2 border-gray-300 rounded-lg text-gray-600 shadow-md h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none font-bold appearance-none"
-							v-model="gender"
-						>
-							<option class="font-bold" value="male">Male</option>
-							<option value="female" class="font-bold inline-block">
-								Female
-							</option>
-							<option value="other" class="font-bold inline-block">
-								Other
-							</option>
-						</select>
-					</div>
-					<label
-						class="inline-flex items-center ml-4 text-sm font-bold"
-						:class="errors.inputChecked && 'check-box-error'"
-					>
-						<input
-							type="checkbox"
-							class="h-5 w-5"
-							v-model="inputChecked"
-							checked
-						/>
-						<span
-							:class="`select-none ml-3 inline-block capitalize ${
-								inputChecked ? 'text-green-500' : 'text-red-500'
-							}`"
-						>
-							I agree All the Trams and conditions
-						</span>
-					</label>
-				</div>
-				<div class="grid-cols-2 mt-4" style="grid-column: 1 / span 2">
-					<button
-						class="btn-gray-lg shadow-xl"
-						type="submit"
-						:disabled="isSubmitting"
-					>
-						<div class="loader" v-if="isSubmitting">
-							<div class="loader-inner ball-pulse">
-								<div></div>
-								<div></div>
-								<div></div>
+							<div class="auth-input-icon">
+								<i class="eva eva-person-outline"></i>
 							</div>
 						</div>
-						<span v-else>Register</span>
-					</button>
-					<span class="block my-3 text-center font-bold text-lg text-gray-800">
-						OR
-					</span>
-					<div class="text-center">
-						<router-link class="auth-route-link" to="/login">
-							Login Now
-						</router-link>
+						<span v-if="errors.first_name" class="auth-input-error mt-3">
+							{{ errors.first_name }}
+						</span>
 					</div>
-				</div>
-			</form>
+					<div class="form-control py-2">
+						<label class="label">
+							<span class="label-text">Last Name</span>
+						</label>
+						<div class="relative">
+							<input
+								placeholder="Deo"
+								type="text"
+								class="auth-input input"
+								v-model="last_name"
+							/>
+							<div class="auth-input-icon">
+								<i class="eva eva-person-outline"></i>
+							</div>
+						</div>
+						<span v-if="errors.last_name" class="auth-input-error mt-3">
+							{{ errors.last_name }}
+						</span>
+					</div>
+					<div class="form-control py-2">
+						<label class="label">
+							<span class="label-text">Email</span>
+						</label>
+						<div class="relative">
+							<input
+								placeholder="user@email.com"
+								type="text"
+								class="auth-input input"
+								name="email"
+								v-model="email"
+							/>
+							<div class="auth-input-icon">
+								<i class="eva eva-email-outline"></i>
+							</div>
+						</div>
+						<span v-if="errors.email" class="auth-input-error mt-3">
+							{{ errors.email }}
+						</span>
+					</div>
+					<div class="form-control py-2">
+						<label class="label">
+							<span class="label-text">Username</span>
+						</label>
+						<div class="relative">
+							<input
+								placeholder="jhone146"
+								type="text"
+								class="auth-input input"
+								v-model="username"
+							/>
+							<div class="auth-input-icon">
+								<i class="eva eva-email-outline"></i>
+							</div>
+						</div>
+						<span v-if="errors.username" class="auth-input-error mt-3">
+							{{ errors.email }}
+						</span>
+					</div>
+					<div class="form-control py-2">
+						<label class="label">
+							<span class="label-text">Password</span>
+						</label>
+						<div class="relative">
+							<input
+								id="password"
+								placeholder="................"
+								:type="showPassword ? 'text' : 'password'"
+								v-model="password"
+								class="auth-input input"
+							/>
+							<div class="auth-input-icon">
+								<i class="eva eva-lock-outline"></i>
+							</div>
+							<div
+								class="absolute right-0 px-3 rounded-lg flex items-center text-2xl leading-5 text-gray-800 bg-white"
+								@click="showPassword = !showPassword"
+								style="top: 2px; bottom: 2px; right: 2px"
+							>
+								<i
+									:class="`eva ${
+										showPassword
+											? 'eva-eye-off-outline'
+											: 'eva-eye-outline'
+									}`"
+								></i>
+							</div>
+						</div>
+						<span v-if="errors.password" class="auth-input-error mt-3">
+							{{ errors.password }}
+						</span>
+					</div>
+					<div class="form-control py-2 mb-5">
+						<label class="label">
+							<span class="label-text">Confirm Password</span>
+						</label>
+						<div class="relative">
+							<input
+								id="confirm-password"
+								placeholder="................"
+								:type="showConfirmPassword ? 'text' : 'password'"
+								v-model="confirmPassword"
+								class="auth-input input"
+							/>
+							<div class="auth-input-icon">
+								<i class="eva eva-lock-outline"></i>
+							</div>
+							<div
+								class="absolute right-0 px-3 rounded-lg flex items-center text-2xl leading-5 text-gray-800 bg-white"
+								@click="showConfirmPassword = !showConfirmPassword"
+								style="top: 2px; bottom: 2px; right: 2px"
+							>
+								<i
+									:class="`eva ${
+										showConfirmPassword
+											? 'eva-eye-off-outline'
+											: 'eva-eye-outline'
+									}`"
+								></i>
+							</div>
+						</div>
+						<span v-if="errors.confirmPassword" class="auth-input-error mt-3">
+							{{ errors.confirmPassword }}
+						</span>
+					</div>
+					<DropDown
+						@selectedItem="selectedItem"
+						:gender="gender"
+						:error="errors.gender"
+					/>
+					<div class="grid-cols-2 py-2" style="grid-column: 1 / span 2">
+						<div class="form-control mb-2 mt-4">
+							<label class="cursor-pointer label justify-center">
+								<div>
+									<input
+										type="checkbox"
+										v-model="inputChecked"
+										checked
+										class="checkbox"
+									/>
+									<span
+										style="transform: scaleX(-1)"
+										class="checkbox-mark"
+									></span>
+								</div>
+								<span
+									:class="[
+										'select-none ml-3 inline-block capitalize',
+										inputChecked
+											? 'text-green-300'
+											: errors.inputChecked
+											? 'text-yellow-400'
+											: 'text-neutral-content'
+									]"
+									>I agree All the Trams and conditions</span
+								>
+							</label>
+						</div>
+					</div>
+					<div class="grid-cols-2 mt-4" style="grid-column: 1 / span 2">
+						<button
+							class="btn-green-lg shadow-xl"
+							type="submit"
+							:disabled="isSubmitting"
+						>
+							<div v-if="isSubmitting" class="loader">
+								<div
+									class="loader-inner auth-loading-spiner semi-circle-spin mx-auto"
+								>
+									<div></div>
+								</div>
+							</div>
+							<span v-else>Register</span>
+						</button>
+
+						<span
+							class="block my-3 text-center font-bold text-lg text-gray-800"
+						>
+							OR
+						</span>
+						<div class="text-center">
+							<router-link class="auth-route-link" to="/login">
+								Login Now
+							</router-link>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
 	</default-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useForm, useField } from 'vee-validate';
 import UserIcon from '../components/utils/user-icon.vue';
 import { registerSchema } from '../utils/schema';
 import { useMutation } from '@urql/vue';
-import gql from 'graphql-tag';
+import DropDown from '../components/utils/drop-down.vue';
 import omit from 'lodash/omit';
 import { useToast } from 'vue-toastification';
 import { REGISTER_USER } from '../typeDefs';
@@ -239,7 +246,7 @@ export default defineComponent({
 			last_name: '',
 			username: '',
 			confirmPassword: '',
-			gender: 'male',
+			gender: '',
 			inputChecked: false
 		};
 		const { handleSubmit, errors, resetForm } = useForm({
@@ -270,6 +277,11 @@ export default defineComponent({
 				}
 			});
 		});
+
+		function selectedItem(val: string) {
+			gender.value = val;
+		}
+
 		return {
 			showPassword,
 			showConfirmPassword,
@@ -283,11 +295,13 @@ export default defineComponent({
 			username,
 			confirmPassword,
 			gender,
-			inputChecked
+			inputChecked,
+			selectedItem
 		};
 	},
 	components: {
-		UserIcon
+		UserIcon,
+		DropDown
 	}
 });
 </script>
