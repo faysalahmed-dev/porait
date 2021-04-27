b818ebfd36b245e32fd8c6e554409cb71bd4b1f7
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-27 17:57:41 +0600

    feat: update authuser cache after image upload
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

--- client/src/resolvers/updateNodes.ts
+++ client/src/resolvers/updateNodes.ts
@@ -1,15 +1,30 @@
-import { AUTH_USER } from './../typeDefs/auth';
 import { UpdatesConfig } from '@urql/exchange-graphcache';
+import { AUTH_USER } from '../typeDefs/auth';
+
 const updateNode: UpdatesConfig = {
 	Mutation: {
 		login(result, __, cache) {
-			cache.updateQuery({ query: AUTH_USER }, () => {
-				return { authUser: result.login };
-			});
+			cache.updateQuery({ query: AUTH_USER }, () => ({ authUser: result.login }));
 		},
 		register(result, __, cache) {
-			cache.updateQuery({ query: AUTH_USER }, () => {
-				return { authUser: result.register };
+			cache.updateQuery({ query: AUTH_USER }, () => ({
+				authUser: result.register
+			}));
+		},
+		uploadImage(result, args, cache) {
+			// @ts-ignore
+			cache.updateQuery({ query: AUTH_USER }, data => {
+				if (!data) return data;
+				return {
+					authUser: {
+						// @ts-ignore
+						...data.authUser,
+						images: {
+							...data.authUser.images,
+							[args.type]: result.uploadImage
+						}
+					}
+				};
 			});
 		}
 	},
