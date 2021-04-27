9ba4e616e3546c64f2dbae77eee922647187cf51
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-27 17:56:51 +0600

    chore: add @urql exchange for file upload
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,14 +1,16 @@
 import { defaultExchanges, ClientOptions, createClient } from '@urql/vue';
-import resolvers from './resolvers/index';
-import updateNodes from './resolvers/updateNodes';
+import { multipartFetchExchange } from '@urql/exchange-multipart-fetch';
 import { devtoolsExchange } from '@urql/devtools';
 import { cacheExchange } from '@urql/exchange-graphcache';
+import resolvers from './resolvers/index';
+import updateNodes from './resolvers/updateNodes';
 
 const clientOpt: ClientOptions = {
 	url: 'http://localhost:8000/graphql',
 	exchanges: [
 		devtoolsExchange,
 		cacheExchange({ resolvers, updates: updateNodes }),
+		multipartFetchExchange,
 		...defaultExchanges
 	],
 	fetchOptions: () => {
