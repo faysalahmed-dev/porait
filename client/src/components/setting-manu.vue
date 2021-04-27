7c83bd544aa777784842947d55b0d5dc96031d25
Faysal Ahmed <fahadfaysal146@gmail.com>
2021-04-27 17:49:04 +0600

    feat: add user setting page
    Signed-off-by: Faysal Ahmed <fahadfaysal146@gmail.com>

---
+++ client/src/components/setting-manu.vue
@@ -0,0 +1,41 @@
+<template>
+	<ul class="menu p-3 text-sm shadow-lg bg-base-100 bg-neutral rounded-box space-y-5">
+		<li>
+			<router-link :to="{ name: 'update-user' }">
+				<i class="eva eva-person-outline eva-lg"></i>
+				General
+			</router-link>
+		</li>
+		<li>
+			<router-link :to="{ name: 'change-password' }">
+				<i class="eva eva-lock-outline eva-lg"></i>
+				Change Password
+			</router-link>
+		</li>
+		<li>
+			<router-link to="/">
+				<i class="eva eva-info-outline eva-lg"></i>
+				Information
+			</router-link>
+		</li>
+		<li>
+			<router-link to="/">
+				<i class="eva eva-bell-outline eva-lg"></i>
+				Notifactions
+			</router-link>
+		</li>
+	</ul>
+</template>
+
+<style scoped>
+ul li a i {
+	display: inline-block;
+	margin-right: 10px;
+}
+.router-link-active {
+	--tw-bg-opacity: 1;
+	background-color: hsla(var(--p, 259 94% 51%) / var(--tw-bg-opacity));
+	--tw-text-opacity: 1;
+	color: hsla(var(--pc, 0 0% 100%) / var(--tw-text-opacity));
+}
+</style>
---
+++ client/src/pages/account-setting.vue
@@ -0,0 +1,17 @@
+<template>
+	<default-layout>
+		<div class="flex py-3 space-x-3">
+			<div class="w-4/12"><SettionManu /></div>
+			<div class="flex-auto rounded-lg bg-neutral"><router-view></router-view></div>
+		</div>
+	</default-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SettionManu from '../components/setting-manu.vue';
+
+export default defineComponent({
+	components: { SettionManu }
+});
+</script>
